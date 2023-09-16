87bd016de516234b3d81e21609b19cf1aebcc23b
Urrea <Surrea1411@gmail.com>
2023-09-16 04:09:56 -0500

    jeje

--- Exercise19/Program.cs
+++ Exercise19/Program.cs
@@ -1,11 +1,12 @@
 ﻿int number = 2, divisible = 0;
+int s = 0, i = 1;
 Console.Write("How many prime numbers do you want to print? ");
 int finalNumber = int.Parse(Console.ReadLine());
 while (number <= finalNumber)
 {
-    for (int i = 1; i <= number; i++)
+    for (int ix = 1; ix <= number; ix++)
     {
-        if (number % i == 0)
+        if (number % ix == 0)
         {
             divisible++;
         }
@@ -17,9 +18,13 @@ while (number <= finalNumber)
     if (divisible == 2)
     {
         Console.Write(" {0,14:N0} is prime", number);
+        s = s + number;
+        i = i + 1;
     }
     divisible = 0;
     number++;
-
 }
+
+Console.WriteLine("\nThe sum is......: ${0,10:N2}", s);
+Console.WriteLine("the average is..: ${0,10:N2} ", s / number);
 Console.ReadKey();
